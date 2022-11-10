28ba59f4d6bca57da68af7450c65bd86b209c773
yavuzberkay <78129426+yavuzberkay@users.noreply.github.com>
2022-11-10 20:40:00 +0300

    Build react app and created public folder in server

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql')
 const cors = require("cors");
 
 app.use(cors());
-app.use(express.json());
+app.use(express.static(path.join(__dirname + "/public")));
 
 const db = mysql.createConnection({
     user: 'sql7544337',
