bef635d7de4e935c5796cd90175580c13eb578a5
irmakozu <irmakozuguzel@sabanciuniv.edu>
2022-12-15 14:16:40 +0300

    Category filtering function working

--- client/src/component/loginadmin.component.js
+++ client/src/component/loginadmin.component.js
@@ -1,54 +1,61 @@
-import React, { useState, useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import Axios from "axios";
-import { adminContext } from "../store/context";
+
+import React, { useState } from "react";
+import Axios from 'axios';
+import { useDispatch, useSelector } from "react-redux";
+
 
 export function LoginAdmin() {
-  window.scrollTo(0, 0);
-  const { setAdmin } = useContext(adminContext);
-  const [adminloginStatus, setAdminLoginStatus] = useState("");
-  const [adminpassword, setAdminPassword] = useState("");
-  const [adminusername, setAdminUsername] = useState("");
-  const navigate = useNavigate();
+    const [input, setInput] = useState("");
+    const [playerList, setPlayerList] = useState([]);
+    const getPlayer = () => {
+      Axios.get(`${process.env.REACT_APP_API_URL}/player`).then((response) => {
+        console.log(response.data)
+        setPlayerList(response.data);
+      });
+    };
 
-  const loginadmin = () => {
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    };
 
-    Axios.post(`${process.env.REACT_APP_API_URL}/loginadmin`, {
-      adminusername: adminusername,
-      adminpassword: adminpassword,
-    }).then((response) => {
-      if (response.data.message) {
-        setAdminLoginStatus(response.data.message);
-      } else {
-        //setAdminLoginStatus(response.data[0].adminusername)
-        console.log(response.data);
-        setAdmin(response.data[0]);
-        window.location = `/adminprofilepage/${adminusername}`;
-      }
-    });
-  };
-  
-  return (
-    <div>
-      <div className="loginadmin">
-        <h1>Admin Login</h1>
-        <input
-          type="text"
-          placeholder="Username..."
-          onChange={(event) => {
-            setAdminUsername(event.target.value);
-          }}
-        />
-        <input
-          type="password"
-          placeholder="Password..."
-          onChange={(event) => {
-            setAdminPassword(event.target.value);
-          }}
-        />
-        <button onClick={loginadmin}> Login </button>
-        <h1>{adminloginStatus}</h1>
-      </div>
-    </div>
-  );
+
+    return (
+        <div>
+            <div className="search">
+                <p></p>
+                <div className="toget">
+                    <form >
+                        Enter keyword
+                        &nbsp;
+                    <input type="text" onChange={handleChange}></input>
+                    </form>
+                &nbsp; &nbsp;
+                <button className="search" onClick={getPlayer}>Show Players</button>
+                </div>
+                <p></p>
+            </div>
+            <div className="teamsList">
+
+
+                {playerList.map((val, key) => {
+                    if (val.playerteam === input) {
+                        return (
+                            <ol className="team" key={val.id} >
+                                <h1 className="stadium">{val.p_num} - {val.playername}</h1>
+                                <p className="playernationality">{val.playernationality}</p>
+                                <p className="playerposition">{val.playerposition}</p>
+                                <p className="playerposition">{val.playerposition}</p>
+                                <p className="playerrating">Position: {val.playerrating}</p>
+                                <p className="playerteam">Position: {val.playerteam}</p>
+
+                                <div>
+                                </div>
+                                
+                            </ol>
+                        );
+                    }
+                })}
+            </div>
+        </div>
+    );
 }
